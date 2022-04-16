dd8c2a7832ac6dc2f2d712bf178834c4fdae14eb
Neothawreww <98629525+Neothawreww@users.noreply.github.com>
2022-04-16 02:13:55 -0500

    Update and rename ITManTokenCrowdsale.sol to BitFuelCrowdsale.sol

--- contracts/BitFuelCrowdsale.sol
+++ contracts/BitFuelCrowdsale.sol
@@ -7,7 +7,7 @@ import "./Crowdsale.sol";
 import "./AllowanceCrowdsale.sol";
 import "./TimedCrowdsale.sol";
 
-contract ITManTokenCrowdsale is AllowanceCrowdsale, TimedCrowdsale {
+contract BitFuelCrowdsale is AllowanceCrowdsale, TimedCrowdsale {
 	constructor(
 		uint256 _rate,
 		address payable _wallet,
